6812616113958dbbf3a9e805279d5d5f5398fe75
Adam Wolanski <adam.wolanski94@gmail.com>
2015-04-21 09:27:52 +0200

    Added tween

--- settings.js
+++ settings.js
@@ -3,12 +3,13 @@ var GameSetup = (function () {
         this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { preload: this.preload, create: this.create });
     }
     GameSetup.prototype.preload = function () {
-        this.game.load.image('phaser', 'phaser.png');
+        this.game.load.image('phaser', 'graphics/phaser.png');
     };
     GameSetup.prototype.create = function () {
         var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'phaser');
         logo.anchor.setTo(0.5, 0.5);
         logo.scale.setTo(0.2, 0.2);
+        this.game.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
     };
     return GameSetup;
 })();
--- settings.ts
+++ settings.ts
@@ -7,14 +7,14 @@ class GameSetup{
         'content',{preload: this.preload, create: this.create});
     }
     preload(){
-        this.game.load.image('phaser', 'phaser.png');
+        this.game.load.image('phaser', 'graphics/phaser.png');
     }
     create(){
-        var logo = this.game.add.sprite
-        (this.game.world.centerX,this.game.world.centerY,'phaser');
-        logo.anchor.setTo(0.5,0.5);
-        logo.scale.setTo(0.2,0.2);
-
+        var logo = this.game.add.sprite(this.game.world.centerX,this.game.world.centerY,'phaser');
+        logo.anchor.setTo(0.5, 0.5);
+        logo.scale.setTo(0.2, 0.2);
+        this.game.add.tween(logo.scale).to
+        ({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
     }
 }
 
